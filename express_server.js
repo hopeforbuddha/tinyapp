67b8c5018f06eb0b108b088eaa8db40f0a30e689
Jacob <jacob.fitzgerald@outlook.com>
2021-06-09 21:26:02 +0000

    fixed bud where you oculdnt get into /urls without registering, created emptyfield checker and duplicate checker to see if there are any issues with email and password inputs

--- express_server.js
+++ express_server.js
@@ -6,17 +6,48 @@ const cookieParser = require('cookie-parser');
 app.set("view engine", "ejs");
 app.use(cookieParser());
 
-const randomStringGenerator = () => {
+const randomStringGenerator = (input) => {
   let result = [];
   const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz123456789";
-  for (let i = 0; i < 6; i++) {
+  for (let i = 0; i < input; i++) {
     result.push(characters.charAt(Math.floor(Math.random() * characters.length)));
   }
   return result.join('');
 }
 
 const bodyParser = require("body-parser");
+const { response } = require("express");
 app.use(bodyParser.urlencoded({extended: true}));
+
+
+
+
+let users = {
+  333: {
+    id: 333,
+    email: "user@example.com",
+    password: "12345"
+  }
+}
+
+const checkDuplicate = (email) => {
+  for (let userID in users) {
+console.log("check duplicate", users[userID].email)
+    if (users[userID].email === email) {
+    console.log("error");
+    return true;
+    }
+  }
+  return false;
+}
+
+const checkEmpty = (req, res) => {
+  if (!req.body.email || !req.body.password) {
+    res.status(400);
+    res.send("Invalidy entry");
+    return
+  }
+}
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -24,7 +55,7 @@ const urlDatabase = {
 };
 
 app.get("/", (req, res) => {
-  res.redirect("/urls");
+  res.redirect("/register");
 }); 
 
 app.get("/urls.json", (req, res) => {
@@ -44,13 +75,14 @@ app.get("/urls/new", (req, res) => {
 })
 
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase, username: req.cookies["username"] };
+  const templateVars = { urls: urlDatabase, users , username: req.cookies.userID };
+  console.log("this is cookie value: ", req.cookies.userID)
   res.render("urls_index", templateVars);
 });
 
 // gets shortURL
 app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = { username: req.cookies["username"], shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  const templateVars = { users, shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   //urlDatabase.keyValues = templateVars
   console.log(req.params)
   res.render("urls_show", templateVars);
@@ -60,7 +92,7 @@ app.get("/urls/:shortURL", (req, res) => {
 app.post("/urls", (req, res) => {
     // Log the POST request body to the console
     // Respond with 'Ok' (we will replace this)ß
-  const shortURL = randomStringGenerator()
+  const shortURL = randomStringGenerator(6)
   const longURL = req.body.longURL;
   urlDatabase[shortURL] = longURL;
   res.redirect(`/urls/${shortURL}`)
@@ -91,10 +123,35 @@ app.post("/login", (req, res) => {
   res.redirect("/urls")
 })
 
-
-
 app.post("/logout", (req, res) => {
   res.clearCookie("username")
   res.redirect("/")
 
 })
+
+app.get("/register", (req, res) => {
+  res.render("urls_register", {users})
+})
+
+app.post("/register", (req, res) => {
+ let userRandomID = randomStringGenerator(3);
+
+
+ if (checkDuplicate(req.body.email)) {
+   res.status(400);
+   res.send("Invalid entry")
+   return
+ }
+ 
+ users[userRandomID] = {
+  id: userRandomID,
+  email: req.body.email,
+  password: req.body.password
+ }
+ res.cookie("userID", userRandomID);
+ //console.log(users)
+ 
+ console.log(users)
+ res.redirect("/")
+})
+
